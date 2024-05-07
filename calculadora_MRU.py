eae5ba900a6620c78f166f82ab944e0853fbd935
Edson <eloepzqu@uns.edu.pe>
2024-05-07 18:49:09 -0500

    Se crea la calculadora sobre MRU

---
+++ calculadora_MRU.py
@@ -0,0 +1,12 @@
+#
+print("Bienvenido a su calculadora de MRU")
+print("Cual es su incognita: ")
+print("1) Distancia")
+print("2) Tiempo")
+print("3) Velocidad")
+incognita = int(input("Ingrese el número de su incognita: "))
+if(incognita==1):
+    print("Hallando DISTANCIA .... : ")
+    tiempo = int(print("Ingrese el tiempo: "))
+    if(tiempo==)
+
