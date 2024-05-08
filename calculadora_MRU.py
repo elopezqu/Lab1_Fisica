b6c4a4dd7f77214467400aa93d51b8bf1917dcf7
Edson <eloepzqu@uns.edu.pe>
2024-05-07 19:37:11 -0500

    Se agrega el metodo para hallar la distancia

--- calculadora_MRU.py
+++ calculadora_MRU.py
@@ -1,4 +1,16 @@
-#
+import time
+
+# Método para hallar la distancia 
+def hallarDistancia():
+    tiempo = int(input("Ingrese el tiempo(segundos): "))
+    if(tiempo > 0):
+        velocidad = int(input("Ingrese la velocidad(metros/segundos): "))
+        if(velocidad > 0):
+            print("Hallando DISTANCIA .... : ")
+            time.sleep(2)
+            print("La distancia es:",velocidad*tiempo,"m")
+
+# Main
 print("Bienvenido a su calculadora de MRU")
 print("Cual es su incognita: ")
 print("1) Distancia")
@@ -6,7 +18,10 @@ print("2) Tiempo")
 print("3) Velocidad")
 incognita = int(input("Ingrese el número de su incognita: "))
 if(incognita==1):
-    print("Hallando DISTANCIA .... : ")
-    tiempo = int(print("Ingrese el tiempo: "))
-    if(tiempo==)
+    print("Se ha seleccionado DISTANCIA")
+    time.sleep(1)
+    hallarDistancia()
+    
+
+
 
