0da7e336083549f314ee257e0d648c0f0b808830
Edson Lopez Quispe <elopezqu@unsa.edu.pe>
2024-05-08 01:41:37 -0500

    Se a optimizado el codigo de la calculadora MRU al reducir if de más

--- calculadora_MRU.py
+++ calculadora_MRU.py
@@ -3,115 +3,59 @@ import time
 
 # Método para hallar la variación de la distancia 
 def hallarDistancia():
-    tiempo = float(input("Ingrese la variación del tiempo(segundos): "))
-    if tiempo > 0:
+    tiempo = float(input("Ingrese la variación del tiempo(segundos): ")) 
+    while(tiempo <= 0):
+        print("El valor ingresado no corresponde a VARIACIÓN DEL TIEMPO")
+        tiempo = float(input("Ingrese la variacion del tiempo(segundos): "))
+    velocidad = float(input("Ingrese la velocidad(metros/segundos): "))
+    while(velocidad <= 0):
+        print("El valor ingresado no corresponde a VELOCIDAD")
         velocidad = float(input("Ingrese la velocidad(metros/segundos): "))
-        if velocidad > 0:
-            print("Hallando VARIACIÓN DE LA DISTANCIA .... : ")
-            time.sleep(2)
-            print("la variación de la distancia es:", velocidad * tiempo, "m")
-        else:
-            while velocidad <= 0:
-                print("El valor ingresado no corresponde a VELOCIDAD")
-                velocidad = float(input("Ingrese la velocidad(metros/segundos): "))
-            print("Hallando VARIACIÓN DE LA DISTANCIA .... : ")
-            time.sleep(2)
-            print("la variación de la distancia es:", velocidad * tiempo, "m")
-    else:
-        while tiempo <= 0:
-            print("El valor ingresado no corresponde a VARIACIÓN DEL TIEMPO")
-            tiempo = float(input("Ingrese la variacion del tiempo(segundos): "))
-        velocidad = float(input("Ingrese la velocidad(metros/segundos): "))
-        if velocidad > 0:
-            print("Hallando VARIACIÓN DE LA DISTANCIA .... : ")
-            time.sleep(2)
-            print("la variación de la distancia es:", velocidad * tiempo, "m")
-        else:
-            while velocidad <= 0:
-                print("El valor ingresado no corresponde a VELOCIDAD")
-                velocidad = float(input("Ingrese la velocidad(metros/segundos): "))
-            print("Hallando VARIACIÓN DE LA DISTANCIA .... : ")
-            time.sleep(2)
-            print("la variación de la distancia es:", velocidad * tiempo, "m")
+    print("Hallando VARIACIÓN DE LA DISTANCIA .... : ")
+    time.sleep(2)
+    print("la variación de la distancia es:", velocidad * tiempo, "m")
 
 
 # Método para hallar el tiempo 
 def hallarTiempo():
     distancia = float(input("Ingrese la variación de la distancia(metros): "))
-    if distancia >= 0:
+    while(distancia <= 0):
+        print("El valor ingresado no corresponde a VARIACIÓN DE LA DISTANCIA")
+        distancia = float(input("Ingrese la variación de la distancia(metros): "))
+    velocidad = float(input("Ingrese la velocidad(metros/segundos): "))
+    while(velocidad <= 0):
+        print("El valor ingresado no corresponde a VELOCIDAD")
         velocidad = float(input("Ingrese la velocidad(metros/segundos): "))
-        if velocidad > 0:
-            print("Hallando VARIACIÓN DEL TIEMPO .... : ")
-            time.sleep(2)
-            print("El tiempo es:", distancia / velocidad, "s")
-        else:
-            while velocidad <= 0:
-                print("El valor ingresado no corresponde a VELOCIDAD")
-                velocidad = float(input("Ingrese la velocidad(metros/segundos): "))
-            print("Hallando VARIACIÓN DEL TIEMPO .... : ")
-            time.sleep(2)
-            print("El tiempo es:", distancia / velocidad, "s")
-    else:
-        while distancia < 0:
-            print("El valor ingresado no corresponde a VARIACIÓN DE LA DISTANCIA")
-            distancia = float(input("Ingrese la variación de la distancia(metros): "))
-        velocidad = float(input("Ingrese la velocidad(metros/segundos): "))
-        if velocidad > 0:
-            print("Hallando VARIACIÓN DEL TIEMPO .... : ")
-            time.sleep(2)
-            print("El tiempo es:", distancia / velocidad, "s")
-        else:
-            while velocidad <= 0:
-                print("El valor ingresado no corresponde a VELOCIDAD")
-                velocidad = float(input("Ingrese la velocidad(metros/segundos): "))
-            print("Hallando VARIACIÓN DEL TIEMPO .... : ")
-            time.sleep(2)
-            print("El tiempo es:", distancia / velocidad, "s")
+    print("Hallando VARIACIÓN DEL TIEMPO .... : ")
+    time.sleep(2)
+    print("El tiempo es:", distancia / velocidad, "s")
 
 # Método para hallar la velocidad 
 def hallarVelocidad():
     distancia = float(input("Ingrese la variación de la variación de la distancia(metros): "))
-    if distancia >= 0:
+    while(distancia <= 0):
+        print("El valor ingresado no corresponde a VARIACIÓN DE LA DISTANCIA")
+        distancia = float(input("Ingrese la variación de la distancia(metros): "))
+    tiempo = float(input("Ingrese la variación del tiempo(segundos): "))
+    while(tiempo <= 0):
+        print("El valor ingresado no corresponde a VARIACIÓN DEL TIEMPO")
         tiempo = float(input("Ingrese la variación del tiempo(segundos): "))
-        if tiempo > 0:
-            print("Hallando VELOCIDAD .... : ")
-            time.sleep(2)
-            print("La velocidad es:", distancia / tiempo, "m/s")
-        else:
-            while tiempo <= 0:
-                print("El valor ingresado no corresponde a VARIACIÓN DEL TIEMPO")
-                tiempo = float(input("Ingrese la variación del tiempo(segundos): "))
-            print("Hallando VELOCIDAD .... : ")
-            time.sleep(2)
-            print("La velocidad es:", distancia / tiempo, "m/s")
-    else:
-        while distancia < 0:
-            print("El valor ingresado no corresponde a VARIACIÓN DE LA DISTANCIA")
-            distancia = float(input("Ingrese la variación de la distancia(metros): "))
-        tiempo = float(input("Ingrese la variación del tiempo(segundos): "))
-        if tiempo <= 0:
-            print("Hallando VELOCIDAD .... : ")
-            time.sleep(2)
-            print("La velocidad es:", distancia / tiempo, "m/s")
-        else:
-            while tiempo <= 0:
-                print("El valor ingresado no corresponde a VARIACIÓN DEL TIEMPO")
-                tiempo = float(input("Ingrese la variación del tiempo(segundos): "))
-            print("Hallando VELOCIDAD .... : ")
-            time.sleep(2)
-            print("La velocidad es:", distancia / tiempo, "m/s")
+    print("Hallando VELOCIDAD .... : ")
+    time.sleep(2)
+    print("La velocidad es:", distancia / tiempo, "m/s")
+
 
 # Main
 print("Bienvenido a su calculadora de MRU")
 print("Cual es su incognita: ")
-print("1) Variación de la variación de la distancia")
+print("1) Variación de la distancia")
 print("2) Variación del tiempo")
 print("3) Velocidad")
 operacion = int(input("Ingrese el número de su incognita: "))
 while(operacion != 1 and operacion != 2 and operacion != 3 ):
     print("!!Número de incognita no disponible!!")
     print("Cual es su incognita: ")
-    print("1) Variación de la variación de la distancia")
+    print("1) Variación de la distancia")
     print("2) Variación del tiempo")
     print("3) Velocidad")
     operacion = int(input("Ingrese el número de su incognita: "))
